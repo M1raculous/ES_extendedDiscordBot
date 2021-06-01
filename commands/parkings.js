8e0c66c02c1b881673225327d6292cd1ca82d4fe
M1raculous <theisbechmann0901@gmail.com>
2021-06-01 20:52:52 +0200

    Fixed spacing

--- commands/parkings.js
+++ commands/parkings.js
@@ -11,19 +11,17 @@ module.exports = {
       `SELECT * FROM user_parkings WHERE identifier = '${id}'`,
       function getParkings(err, result) {
         if (result) {
-          const parkingsEmbed = new MessageEmbed()
+          const parkingsEmbed = new MessageEmbed();
           parkingsEmbed.setColor("#1874ed");
           for (var i = 0; i < result.length; i++) {
             var n = i + 1;
-  
+
             // console.log(result[i]["id"] || 'No id found!')
-            parkingsEmbed.addFields(
-            {
+            parkingsEmbed.addFields({
               name: "Parking Zone #" + n,
               value: `**ID:**\n ${result[i]["id"]}\n**Garage:**\n  ${result[i]["garage"]}\n **Zone**\n ${result[i]["zone"]} `,
               inline: true,
-            }
-            )
+            });
           }
           parkingsEmbed.setTimestamp();
           message.channel.send(parkingsEmbed);
--- commands/plates.js
+++ commands/plates.js
@@ -1,11 +1,11 @@
-const { MessageEmbed } = require('discord.js');
-const config = require('../secure/configs/main.json');
+const { MessageEmbed } = require("discord.js");
+const config = require("../secure/configs/main.json");
 
 module.exports = {
-    name: 'plate',
-    description: 'Template!',
-    async execute(client, message, args, con) {
-        if (!message.member.roles.cache.find((r) => r.id === "823882801115693086"))
+  name: "plate",
+  description: "Template!",
+  async execute(client, message, args, con) {
+    if (!message.member.roles.cache.find((r) => r.id === "823882801115693086"))
       return;
 
     let id = args[0];
@@ -16,31 +16,34 @@ module.exports = {
         if (err) return console.error(err);
         if (typeof err === "undefined") throw err;
         if (result) {
-        const vehicleInformation = new MessageEmbed()
-        vehicleInformation.setColor('RANDOM')
-        for (var i = 0; i < result.length; i++) {
-          var n = i + 1;
-          // console.log(n + " plate: " + result[i]["plate"]);
+          const vehicleInformation = new MessageEmbed();
+          vehicleInformation.setColor("RANDOM");
+          for (var i = 0; i < result.length; i++) {
+            var n = i + 1;
+            // console.log(n + " plate: " + result[i]["plate"]);
 
-        vehicleInformation.addFields(
-          {
-            name: `Information om køretøj ${n}`,
-            value: "**Nummerplade**\n " + result[i]["plate"] + "\n **Type**\n " + result[i]["type"],
-            inline: true,
+            vehicleInformation.addFields({
+              name: `Information om køretøj ${n}`,
+              value:
+                "**Nummerplade**\n " +
+                result[i]["plate"] +
+                "\n **Type**\n " +
+                result[i]["type"],
+              inline: true,
+            });
           }
-          )
-        }
-        vehicleInformation.setFooter(`requested by: ${message.author.tag} || ${message.author.id}`)
-        vehicleInformation.setTimestamp()
-        message.channel.send(vehicleInformation)
-        // console.log(results)
-        // console.log(result[1].plate)
-        // console.log(result[i]['plate'])
+          vehicleInformation.setFooter(
+            `requested by: ${message.author.tag} || ${message.author.id}`
+          );
+          vehicleInformation.setTimestamp();
+          message.channel.send(vehicleInformation);
+          // console.log(results)
+          // console.log(result[1].plate)
+          // console.log(result[i]['plate'])
         } else {
-            message.channe.send('Du glemte vist noget, hva!?')
+          message.channe.send("Du glemte vist noget, hva!?");
         }
       }
     );
-    
-    }
+  },
 };
